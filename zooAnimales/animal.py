60c93e694fed033ef6624c79fb70e6e044ae862f
sidiazr <sidiazr@unal.edu.co>
2024-12-16 19:39:59 -0500

    Intento 3

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -20,15 +20,17 @@ class Animal:
         from zooAnimales.mamifero import Mamifero
         from zooAnimales.pez import Pez
         from zooAnimales.reptil import Reptil
-        return f"Mamíferos: {Mamifero.cantidadMamiferos()} \nAves: {Ave.cantidadAves()} \nReptiles: {Reptil.cantidadReptiles()} \nPeces: {Pez.cantidadPeces()} \nAnfibios: {Anfibio.cantidadAnfibios()}"
+        return f"Mamiferos : {Mamifero.cantidadMamiferos()}\nAves : {Ave.cantidadAves()}\nReptiles : {Reptil.cantidadReptiles()}\nPeces : {Pez.cantidadPeces()}\nAnfibios : {Anfibio.cantidadAnfibios()}"
 
     
     def __str__(self):
         if self._zona == None:
-            return f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, y mi genero es {self._genero}"
+            return f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self.getHabitat()} y mi genero es {self._genero}"
         else:
-            return f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, y mi genero es {self._genero}, la zona en la que me ubico es {self._zona}, en el {self._zona.getZoo()}"
+            return f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self.getHabitat()} y mi genero es {self._genero}, la zona en la que me ubico es {self._zona}, en el {self._zona.getZoo()}"
         
+    def toString(self):
+        return self.__str__()
     
     def getNombre(self):
         return self._nombre
